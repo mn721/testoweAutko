c68586a1c662187f41e470ad64ed88f8c98bab12
klaudia <ksawicka2004.ks@gmail.com>
2025-05-11 23:59:17 +0200

    wstępne, niedokończone GUI z wyświetlanym info, do poprawki

--- Assets/carScript.cs
+++ Assets/carScript.cs
@@ -56,8 +56,6 @@ public class carScript : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Q) && currentGear > Gear.R)
             currentGear--;
-            
-        
     }
 
     void FixedUpdate()
@@ -158,13 +156,13 @@ public class carScript : MonoBehaviour
         return true;
     }
 
-    void OnGUI()
+    /*void OnGUI()
     {
         float speed = rigid.linearVelocity.magnitude * 3.6f;
         GUI.Label(new Rect(250, 50, 200, 20), "Predkosc: " + speed.ToString("F1") + " km/h");
         GUI.Label(new Rect(250, 90, 200, 20), "Bieg: " + currentGear.ToString());
         GUI.Label(new Rect(250, 130, 200, 20), "RPM: " + engineRPM.ToString("F0"));
-    }
+    }*/
 
     void SetWheelFriction(WheelCollider wheel)
     {
